8527079170d103bb3c7e1d7491906b37b90f35c4
ErikBoesen <me@erikboesen.com>
2019-08-26 00:51:21 -0400

    Add simple main.py

---
+++ gmbots/main.py
@@ -0,0 +1,23 @@
+import requests
+
+
+class Bot:
+    def __init__(self, slug: str, api_key: str):
+        self.slug = slug
+        self.api_key = api_key
+
+    def get(self, params: dict = {}):
+        """
+        Make a GET request to the API.
+
+        :param params: dictionary of custom params to add to request.
+        """
+        params.update({
+            'apikey': self.api_key,
+        })
+        request = requests.get(self.API_TARGET, params=params)
+        if request.ok:
+            return request.json()
+        else:
+            # TODO: Can we be more helpful?
+            raise Exception('API request failed. Received:\n' + request.text)
