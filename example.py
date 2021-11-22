0e6f1d60c0c1e754eb615c416f789f7d60d90f69
ErikBoesen <me@erikboesen.com>
2021-11-22 17:05:58 -0500

    Add .instances() call to example script

--- example.py
+++ example.py
@@ -6,5 +6,7 @@ GROUP_ID = 49940116
 # "bot" name can be whatever is most convenient for your program
 bot = mebots.Bot("test", os.environ["BOT_TOKEN"])
 
+# Get all bot instances
+print(bot.instances())
 # Given a group's ID, get the ID of the bot instance in that group
 print(bot.instance(GROUP_ID).id)
