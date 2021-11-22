cfc9297f3d48245a9a6c504c17b861e891b6e65a
ErikBoesen <me@erikboesen.com>
2021-11-22 17:05:38 -0500

    Bump to 0.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='mebots',
-      version='0.3.1',
+      version='0.3.2',
       description='Library for running GroupMe bots with the MeBots API toolkit.',
       url='https://github.com/MeBots/mebotspy',
       author='Erik Boesen',
